16616e571357233be57c3fad0d1cd5834e4c0d3b
jwerba <julian.werba@gmail.com>
2015-11-06 15:18:37 -0200

    TDD dar puño

--- spec/fatality_spec.rb
+++ spec/fatality_spec.rb
@@ -7,4 +7,11 @@ describe Juego do
 	juego.energia1.should == 100
 	juego.energia2.should == 100
 	end
+
+	it "Jugador1 da un punio y Energia del Jugador2 en 95" do
+	juego = Juego.new
+	juego.dar_punio_jugador("jugador1")
+	juego.energia1.should == 100
+	juego.energia2.should == 95
+	end
 end
